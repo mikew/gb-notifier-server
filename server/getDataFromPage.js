8ec423ea0bc9e5ce01e9771d577ed582749472c0
Mike Wyatt <wyatt.mike@gmail.com>
2016-03-10 17:15:32 -0400

    add support for upcoming stream

--- server/getDataFromPage.js
+++ server/getDataFromPage.js
@@ -9,15 +9,27 @@ export default function getDataFromPage (markup) {
       .replace(')', '')
       .replace('screen_medium', 'scale_super')
 
-  const $link = $headerPromo.find('a.ellipsis')
+  let $link = $headerPromo.find('a.ellipsis')
+  if ($link.length === 0) {
+    $link = $headerPromo.find('p')
+  }
+
   const url = $link.attr('href')
-  const title = $link.text()
-  const isStreamLive = $headerPromo.is('.live')
+  const title = $link.text().trim()
+  let isStreamLive = $headerPromo.is('.live')
+  let startsAt
+
+  const $countdown = $headerPromo.find('.js-time-countdown')
+  if ($link.length) {
+    startsAt = parseInt($countdown.text().trim(), 10)
+    isStreamLive = false
+  }
 
   return {
     image,
     url,
     title,
     isStreamLive,
+    startsAt,
   }
 }
